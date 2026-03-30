392bc6bef9ea87e10db8e93a21b591ce1b57e1c1
raul <raulperalcortes13@mail.com>
2026-03-30 20:32:04 +0200

    mod kanban

--- app/Http/Controllers/KanbanController.php
+++ app/Http/Controllers/KanbanController.php
@@ -7,49 +7,48 @@ use Illuminate\Http\Request;
 
 class KanbanController extends Controller
 {
-    
+
     public function index()
     {
         $kanban = Kanban::get();
-
-        return view('kanban.index', compact('kanban'));
     }
-    
 
-   
+
+
     public function create()
     {
-        return view('kanban.create');
+
     }
 
-   
+
     public function store(Request $request)
     {
         $kanban = new Kanban();
         $kanban->name = $request->input('name');
         $kanban->save();
 
-        return redirect()->route('kanban.index');
+
     }
 
     public function show(Kanban $kanban)
     {
-        
+
     }
 
-    
+
     public function edit(Kanban $kanban)
     {
         //
     }
 
-    
+
     public function update(Request $request, Kanban $kanban)
     {
-        //
+        $kanban->name = $request->input('name');
+        $kanban->save();
     }
 
-    
+
     public function destroy(Kanban $kanban)
     {
         //

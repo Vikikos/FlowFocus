84fc3ea4e5641206ad09887c31734b06599023a5
Vikikos <viktoriakosarova272@gmail.com>
2026-04-05 22:37:48 +0200

    MCR_Calendar

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Chronometer::class);
     }
+    public function calendars(): HasMany
+    {
+        return $this->hasMany(Calendar::class);
+    }
+
 }
